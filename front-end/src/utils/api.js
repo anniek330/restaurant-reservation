151f9a5f0e556ff14da0b6a0b9c7db206749a473
anniek330 <139556514+anniek330@users.noreply.github.com>
2023-12-18 13:23:42 -0500

    Update api.js

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -165,3 +165,15 @@ export async function removeReservationFromTable(table_id, reservation_id, signa
   };
   return await fetchJson(url, options, []);
 }
+
+//remove res_id from a table "finish the table"
+export async function removeReservationFromTable(table_id, reservation_id, signal) {
+  const url = new URL(`${API_BASE_URL}/tables/${table_id}/seat`);
+  const options = {
+    method: "DELETE",
+    headers,
+    body: JSON.stringify({ data: { reservation_id } }),
+    signal,
+  };
+  return await fetchJson(url, options, []);
+}
